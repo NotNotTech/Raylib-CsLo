9ca36ae72db32a4c9c8a3d8ae59287de818b2338
JasonS <jasons@novaleaf.com>
2021-11-18 19:00:13 -0800

    bindings include all api's in raylib.dll

--- binding-gen/regen.ps1
+++ binding-gen/regen.ps1
@@ -8,12 +8,17 @@ pushd ..\sub-modules\ClangSharp\
 dotnet build -c Release
 popd
 # raylib
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file raylib.h --methodClassName Raylib
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file raylib.h --methodClassName Raylib --exclude PI DEG2RAD RAD2DEG
 # raymath
 dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file raymath.h --methodClassName RayMath
 robocopy "./raylib-4.0.0_win64_msvc16/lib" "..\Raylib-CsLo\" raylib.dll
-#hack: replace malformed autogen content
+##hack: replace malformed autogen content
 $replaceFile = '../Raylib-CsLo/autogen/bindings/RayMath.cs'
 (Get-Content $replaceFile).replace('.operator=', '=') | Set-Content $replaceFile
+# rlgl
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file rlgl.h --methodClassName RlGl
+# raygui
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file raygui.h --methodClassName RayGui
+
 
 popd
