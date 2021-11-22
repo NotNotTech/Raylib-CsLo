af5d08cdbb21f4a40070c7254899370f13859fd4
JasonS <jasons@novaleaf.com>
2021-11-22 14:52:17 -0800

    workflow for generating builds for raygui and other extras

--- binding-gen/regen.ps1
+++ binding-gen/regen.ps1
@@ -8,22 +8,27 @@ pushd ..\sub-modules\ClangSharp\
 dotnet build -c Release
 popd
 " ################ # raylib"
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ./raylib-4.0.0_win64_msvc16/include/ --file raylib.h --methodClassName Raylib --libraryPath raylib.dll --exclude PI DEG2RAD RAD2DEG
+$RaylibReleaseDir = "./raylib-4.0.0_win64_msvc16/include/"
+$RaylibSrc = "..\sub-modules\raylib\src"
+$RaylibExtrasSrc = "../sub-modules/raylib/src/extras/"
+$RayBin = "..\sub-modules\bin\Release.DLL\" 
+$RayGuiSrc = "..\sub-modules\raygui"
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RaylibSrc" --file raylib.h --methodClassName Raylib --libraryPath raylib.dll --exclude PI DEG2RAD RAD2DEG
 " ################ # raymath"
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ./raylib-4.0.0_win64_msvc16/include/ --file raymath.h --methodClassName RayMath --libraryPath raylib.dll
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RaylibSrc" --file raymath.h --methodClassName RayMath --libraryPath raylib.dll
 #robocopy "./raylib-4.0.0_win64_msvc16/lib" "..\Raylib-CsLo\" raylib.dll
-robocopy "." "..\Raylib-CsLo\" raylib.dll
+robocopy "$RayBin" "..\Raylib-CsLo\" raylib.dll
 ##hack: replace malformed autogen content
 $replaceFile = '../Raylib-CsLo/autogen/bindings/RayMath.cs'
 (Get-Content $replaceFile).replace('.operator=', '=') | Set-Content $replaceFile
 " ################ # rlgl"
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ./raylib-4.0.0_win64_msvc16/include/ --file rlgl.h --methodClassName RlGl --libraryPath raylib.dll
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RaylibSrc" --file rlgl.h --methodClassName RlGl --libraryPath raylib.dll
 " ################ # raygui"
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ./raylib-4.0.0_win64_msvc16/include/ --file raygui.h --methodClassName RayGui --libraryPath raygui.dll
-robocopy "." "..\Raylib-CsLo\" raygui.dll
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RayGuiSrc" --file raygui.h --methodClassName RayGui --libraryPath raygui.dll --include-directory "$RaylibSrc"
+robocopy "$RayBin" "..\Raylib-CsLo\" raygui.dll
 " ################ # physac"
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ../sub-modules/raylib/src/extras/ --include-directory ../sub-modules/raylib/src/ --file physac.h --methodClassName Physac --libraryPath physac.dll
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RaylibExtrasSrc" --include-directory ../sub-modules/raylib/src/ --file physac.h --methodClassName Physac --libraryPath physac.dll
 " ################ # Easings "
-dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory ../sub-modules/raylib/src/extras/ --include-directory ../sub-modules/raylib/src/ --file easings.h --methodClassName Easings --exclude EaseElasticInOut PI DEG2RAD RAD2DEG
+dotnet ..\sub-modules\ClangSharp\artifacts\bin\sources\ClangSharpPInvokeGenerator\Release\net6.0\ClangSharpPInvokeGenerator.dll @gen-raylib.rsp --file-directory "$RaylibExtrasSrc" --include-directory ../sub-modules/raylib/src/ --file easings.h --methodClassName Easings --exclude EaseElasticInOut PI DEG2RAD RAD2DEG
 
 popd
---
+++ sub-modules/premake5.lua
@@ -0,0 +1,95 @@
+newoption 
+{
+   trigger = "opengl43",
+   description = "use OpenGL 4.3"
+}
+
+workspace "RaylibDLLs"
+	configurations { "Debug","Debug.DLL", "Release", "Release.DLL" }
+	platforms { "x64"}
+
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+		
+	filter "configurations:Debug.DLL"
+		defines { "DEBUG" }
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter "configurations:Release.DLL"
+		defines { "NDEBUG" }
+		optimize "On"	
+		
+	filter { "platforms:x64" }
+		architecture "x86_64"
+		
+	targetdir "bin/%{cfg.buildcfg}/"
+	
+	defines{"PLATFORM_DESKTOP"}
+	if (_OPTIONS["opengl43"]) then
+		defines{"GRAPHICS_API_OPENGL_43"}
+	else
+		defines{"GRAPHICS_API_OPENGL_33"}
+	end
+	
+project "raylib"
+		filter "configurations:Debug.DLL OR Release.DLL"
+			kind "SharedLib"
+			defines {"BUILD_LIBTYPE_SHARED"}
+			
+		filter "configurations:Debug OR Release"
+			kind "StaticLib"
+			
+		filter "action:vs*"
+			defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+			links {"winmm"}
+			
+		filter "action:gmake*"
+			links {"pthread", "GL", "m", "dl", "rt", "X11"}
+			
+		filter{}
+		
+		location "build"
+		language "C++"
+		targetdir "bin/%{cfg.buildcfg}"
+		cppdialect "C++17"
+		
+		includedirs { "raylib/src", "raylib/src/external/glfw/include"}
+		vpaths 
+		{
+			["Header Files"] = { "raylib/src/**.h"},
+			["Source Files/*"] = {"raylib/src/**.c"},
+		}
+		files {"raylib/src/*.h", "raylib/src/*.c"}
+		
+project "raygui"
+	kind "SharedLib"
+	location "raygui"
+	language "C"
+	targetdir "bin/%{cfg.buildcfg}"
+	
+	includedirs {"src"}
+	vpaths 
+	{
+		["Header Files"] = { "**.h"},
+		["Source Files"] = {"**.c", "**.cpp"},
+	}
+	files {"raygui/**.c", "raygui/**.cpp", "raygui/**.h"}
+
+	links {"raylib"}
+	
+	includedirs { "raygui", "raylib/src" }
+	defines{"PLATFORM_DESKTOP", "GRAPHICS_API_OPENGL_33"}
+	
+	filter "action:vs*"
+		defines{"_WINSOCK_DEPRECATED_NO_WARNINGS", "_CRT_SECURE_NO_WARNINGS", "_WIN32"}
+		dependson {"raylib"}
+		links {"raylib.lib", "winmm", "kernel32"}
+		libdirs {"bin/%{cfg.buildcfg}"}
+		
+	filter "action:gmake*"
+		links {"pthread", "GL", "m", "dl", "rt", "X11"}
